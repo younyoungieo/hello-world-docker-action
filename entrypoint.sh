0f450cbdb9fce744d186c1c3437dc36c06b7a038
younyoung <00younyoung@naver.com>
2025-08-06 15:28:33 +0900

    $GITHUB_OUTPUT 관련 수정

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,11 @@ GREETING="Hello, $INPUT_WHO_TO_GREET!"
 # Use workflow commands to do things like set debug messages
 echo "::notice file=entrypoint.sh,line=7::$GREETING"
 
-# Write outputs to the $GITHUB_OUTPUT file
-echo "time=$(date)" >>"$GITHUB_OUTPUT"
+# Write outputs to the $GITHUB_OUTPUT file (only if running in GitHub Actions)
+if [ -n "$GITHUB_OUTPUT" ]; then
+  echo "time=$(date)" >>"$GITHUB_OUTPUT"
+else
+  echo "time=$(date)"
+fi
 
 exit 0
